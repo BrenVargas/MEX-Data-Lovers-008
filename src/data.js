d385cf499dda734d2b4480f5601533a81eea2c39
BrenVargas <brendiux.49@gmail.com>
2019-07-04 23:15:14 -0500

    Subiendo cambios al html, css, y js

--- src/data.js
+++ src/data.js
@@ -8,32 +8,3 @@ const example = () => {
 window.example = example;
 
 // console.log(POKEMON)
-
-const root = document.getElementById("root");
-
-const data = POKEMON.pokemon;
-
-let str = " ";
-
-data.forEach(element => {
-  let id = element.id;
-  let num = element.origin;
-  let name = element.name;
-  let img = element.img;
-  let type = element.type;
-  let weaknesses = element.weaknesses;
-
-  str += `
-  <h2>${id}</h2>
-<h1 class>${name}</h1>
-    <p>${type}</p>
-    <p>${weaknesses}</p>
-    <img src="${img}" alt=""></img>`;
-});
-
-root.innerHTML = str;
-
-// data.forEach(element => {
-//   let id: element.id  num, name, img, type, weaknesses
-
-// })
