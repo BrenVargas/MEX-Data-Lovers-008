1f031ae238eff6fac4064da3e3746c3333afdca9
BrenVargas <brendiux.49@gmail.com>
2019-07-08 14:43:04 -0500

    agregamos tarjetas moviles

--- src/main.js
+++ src/main.js
@@ -2,57 +2,61 @@ const data = POKEMON.pokemon;
 
 // document.getElementById("select-type").addEventListener("click", () => {
 
-
 const root = document.getElementById("root");
 
+const printPokemons = data => {
+  let str = " ";
 
-const printPokemons = (data) => {
-let str = " ";
+  data.forEach(element => {
+    let pokeId = element.id;
+    let pokeNum = element.num;
+    let pokeName = element.name;
+    let pokeImg = element.img;
+    let pokeType = element.type;
+    let pokeWeaknesses = element.weaknesses;
+    let pokeNextEvolution = element.next_evolution;
 
-data.forEach(element => {
-  let pokeId = element.id;
-  let pokeNum = element.num;
-  let pokeName = element.name;
-  let pokeImg = element.img;
-  let pokeType = element.type;
-  let pokeWeaknesses = element.weaknesses;
-  let pokeNextEvolution = element.next_evolution;
+    str += `
 
-  str += `
-  <h2>${pokeName}</h2> 
-  <img src="${pokeImg}" alt=""></img>
-  <p>${pokeType}</p>  
-  <p>${pokeWeaknesses}</p> 
-  
+<div class="flip-container">
+
+<div class="card">
+
+
+<div class="front">
+ <img src="${pokeImg}" alt=""></img>
+ <h2>${pokeName}</h2>
+ </div>
+ <div class="back">
+ <p>Núm. de Pokémon: ${pokeId}</p>
+ <p>Tipo de pokémon: ${pokeType}</p>
+ <p>Debilidades: ${pokeWeaknesses}</p>
+ </div>
+ </div>
+ </div>
+
  
+      
+   
   `;
-})
+  });
 
-root.innerHTML = str;
-}
+  root.innerHTML = str;
+};
 
-// printPokemons(data);  
-
-
+// printPokemons(data);
 
 // })
 
 const selectType = document.getElementById("select-type");
 
 selectType.addEventListener("change", () => {
-  const selectUser = selectType.value
+  const selectUser = selectType.value;
   // const pokeFilter = data.filter(tierra => tierra.type.includes(selectUser));
   const pokeFilter = data.filter(pokemones => pokemones.type[0] == selectUser);
   console.log(pokeFilter);
-  printPokemons(pokeFilter)
-})
-
-
-
-
-
-    
+  printPokemons(pokeFilter);
+});
 
 /* <h2>ID: ${pokeId}</h2>
   <h2>NÚM: ${pokeNum}</h2> */
-
