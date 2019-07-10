bd7e2a30f50430185eb5c06fa2311013b4b6633d
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-07-10 08:42:30 -0500

    Ya ordena en todos los campos y ya es funcion pura

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-
+//Filtar data 
 const pokeFil = (data, condition) => {
   
   let pokeTrueCondition = data.filter(dataPoke => dataPoke.type[0] == condition || dataPoke.type[1] == condition || dataPoke[2] == condition)
@@ -12,3 +12,41 @@ const pokeFil = (data, condition) => {
 
 window.pokeFil = pokeFil ;
 
+//Ordenar data filtrada
+
+const sortData = (data, sortBy) => {
+  let orderAsODes = data.sort((a,b) => {
+    if(sortBy == "name"){
+    if (a.name > b.name) {
+      return 1;
+    };
+    if (a.name < b.name) {
+      return -1;
+    }
+    return 0;
+  }
+
+  if (sortBy == "id") {
+    if (a.id > b.id) {
+      return 1;
+    }
+    if (a.id < b.id) {
+      return -1;
+    }
+    return 0;
+  }
+
+  if (sortBy == "weaknesses") {
+    if (a.weaknesses > b.weaknesses) {
+      return 1;
+    }
+    if (a.weaknesses < b.weaknesses) {
+      return -1;
+    }
+    return 0;
+  }
+  })
+  return orderAsODes;
+};
+
+window.sortData = sortData;
--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 const data = POKEMON.pokemon;
 
+//Código para filtrar por tipos
 let imSelection = document.getElementById("select-type");
 imSelection.addEventListener("click", () => {
 let imValue = imSelection.options[imSelection.selectedIndex].value;
-console.log(typeof imValue);
-console.log(imValue);
+//console.log(typeof imValue);
+//console.log(imValue);
 
-let pokeFilterData = window.pokeFil(data, imValue)
-
-console.log(pokeFilterData);
+let pokeFilterData = window.pokeFil(data, imValue);
+ 
+//console.log(pokeFilterData);
 
 const root = document.getElementById("root");
 
@@ -17,7 +18,6 @@ const root = document.getElementById("root");
   
   pokeFilterData.forEach(element => {
     let pokeId = element.id;
-    let pokeNum = element.num;
     let pokeName = element.name;
     let pokeImg = element.img;
     let pokeType = element.type;
@@ -40,11 +40,57 @@ const root = document.getElementById("root");
     </center></div>
     `;
   })
-  root.innerHTML = str;
+  root.innerHTML = str;   
   
-
+  //Ordenar data filtrada
+  
+  let imOrdenation = document.getElementById("select-order");
+  imOrdenation.addEventListener("click", () => {
+  let imValueOrder = (imOrdenation.options[imOrdenation.selectedIndex].value);
+  console.log(imValueOrder);
+  console.log(typeof imValueOrder);
+  
+  
+  //Crear parametro sortBy
+  
+  
+  
+  
+  let pokeDataOrder = window.sortData(pokeFilterData, imValueOrder);
+  console.log(pokeDataOrder);
+  
+  /*const root = document.getElementById("root");
+  
+  
+    let str = " ";
     
+    pokeDataOrder.forEach(element => {
+      let pokeId = element.id;
+      let pokeName = element.name;
+      let pokeImg = element.img;
+      let pokeType = element.type;
+      let pokeWeaknesses = element.weaknesses;
+      let pokeNextEvolution = element.next_evolution;
+    
+      str += `
+      <div class="flip-container"><center>
+      <div class="card">
+      <div class="front">
+      <img src="${pokeImg}" alt=""></img>
+      <h2>${pokeName}</h2>
+      </div>
+      <div class="back">
+      <p>Núm. de Pokémon: ${pokeId}</p>
+      <p>Tipo de pokémon: ${pokeType}</p>
+      <p>Debilidades: ${pokeWeaknesses}</p>
+      </div>
+      </div>
+      </center></div>
+      `;
+    })
+    root.innerHTML = str;
+  */
   
+  })
 });
-
 
